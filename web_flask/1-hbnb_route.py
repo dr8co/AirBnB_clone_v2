d67c538c58f59c8df6e9be3c1736f49dc8426e35
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-20 17:34:40 +0300

    Start a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0, port 5000
+Routes:
+    '/': display “Hello HBNB!”
+    '/hbnb': display “HBNB”
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays “HBNB”"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
