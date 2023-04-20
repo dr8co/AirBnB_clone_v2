d9a7c4b05ba1c092fd028381f726a577180bff8d
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-20 17:28:26 +0300

    Start a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0:5000
+Routes:
+    '/': display “Hello HBNB!”
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
