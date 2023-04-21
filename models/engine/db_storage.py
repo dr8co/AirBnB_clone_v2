a1a802b4cc6fd63c713961690c8f2d5601e1a468
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-21 19:43:38 +0300

    Optimize imports

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,16 +3,17 @@
 DbStorage class which is necessary for handling files
 in our database"""
 from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
+
 from sqlalchemy import (create_engine)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from models.amenity import Amenity
 from models.base_model import Base
-from models.state import State
 from models.city import City
-from models.user import User
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
+from models.state import State
+from models.user import User
 
 
 class DBStorage:
