571a222fc3996ed2dc7f83876ef92705215b4109
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-21 19:19:57 +0300

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -22,9 +22,19 @@ def states():
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with iformation about <id>, if it exists"""
+    """Displays an HTML page with information about <id>, if it exists"""
     states = storage.all("State")
     for state in states.values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
