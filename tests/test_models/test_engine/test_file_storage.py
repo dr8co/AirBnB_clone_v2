9c81c52607d36b16ceae765a29713873f55d6121
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-21 19:01:05 +0300

    Update tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,8 +4,6 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
-import sys
-from unittest.mock import patch
 
 
 class test_fileStorage(unittest.TestCase):
@@ -13,7 +11,6 @@ class test_fileStorage(unittest.TestCase):
 
     def setUp(self):
         """ Set up test environment """
-        os.environ['HBNB_TYPE_STORAGE'] = "file"
         del_list = []
         for key in storage._FileStorage__objects.keys():
             del_list.append(key)
@@ -34,7 +31,6 @@ class test_fileStorage(unittest.TestCase):
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
-        new.save()
         for obj in storage.all().values():
             temp = obj
         self.assertTrue(temp is obj)
@@ -67,7 +63,7 @@ class test_fileStorage(unittest.TestCase):
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
-        new.save()
+        storage.save()
         storage.reload()
         for obj in storage.all().values():
             loaded = obj
@@ -103,9 +99,8 @@ class test_fileStorage(unittest.TestCase):
         new = BaseModel()
         _id = new.to_dict()['id']
         for key in storage.all().keys():
-            print(key)
             temp = key
-        # self.assertEqual(temp, 'BaseModel' + '.' + _id)
+        self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
