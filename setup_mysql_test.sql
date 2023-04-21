203a98d09c30ca9adce5efda97d7cfaa2fcef4ce
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-21 19:32:22 +0300

    Update SQL files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,6 @@
 -- It defines our test database hbnb_test_db and our test user hbnb_test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
 
